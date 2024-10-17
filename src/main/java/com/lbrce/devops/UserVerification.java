4210f63cbbecab8456714af91af5108b11e2e9ec
KesavaChari-V <chari21761a0560@gmail.com>
2024-10-17 10:56:34 +0530

    Add files via upload

---
+++ src/main/java/com/lbrce/devops/UserVerification.java
@@ -0,0 +1,14 @@
+package com.lbrce.devops;
+
+public class UserVerification {
+	public static void check() {
+		System.out.println("Check Method");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		check();
+
+	}
+
+}
---
+++ src/test/java/com/lbrce/devops/UserVerificationTest.java
@@ -0,0 +1,15 @@
+package com.lbrce.devops;
+
+public class UserVerificationTest {
+
+	public void check() {
+		System.out.println("Testing.....");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Class");
+		UserVerificationTest obj = new UserVerificationTest();
+		obj.check();
+	}
+
+}
